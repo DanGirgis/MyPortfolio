c9103395d5f86176b390e27da18c13f7783126c0
DanGirgi <92120787+DanGirgi@users.noreply.github.com>
2022-03-21 04:55:41 +0200

    Add project files.

---
+++ Core/Entities/Owner.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace Core.Entities
+{
+   public class Owner : EntityBase
+    {
+        public string FullName { get; set; }
+        public string Profil { get; set; }
+        public string Avatar { get; set; }
+        public Address Address { get; set; }
+
+    }
+}
---
+++ Core/Entities/ProtflioItem.cs
@@ -0,0 +1,9 @@
+﻿namespace Core.Entities
+{
+    public class ProtflioItem :EntityBase
+    {
+        public string NameProject { get; set; }
+        public string Description { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
